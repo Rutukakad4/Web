bc38ad85d9096fa5c33c9af5b8d4d779acd937a9
Rutuja Kakad <rutukakad29@gmail.com>
2025-01-26 12:18:00 +0530

    upload

---
+++ javascript/Operator/combined.js
@@ -0,0 +1,8 @@
+let length=parseFloat(prompt("Enter length"));
+let width=parseFloat(prompt("Enter width"));
+
+let perimeter=2*(length+width);
+let area=length*width;
+console.log("Perimeter:",perimeter);
+console.log("Area",area);
+console.log("Is perimeter greater than area?",perimeter>area);
